3c405ddf85bc21fad5012fb0ba18c8b6c80bd6b1
Sunil Mahto <sunil@MacBook-Air.local>
2023-01-23 09:52:41 +0530

    ok

--- first.c
+++ first.c
@@ -3,4 +3,29 @@ int main()
 { int x
     printf("Hello\n");
     return 0;
+}#include <stdio.h>
+int main()
+{ int x
+    printf("Hello\n");
+    return 0;
+}#include <stdio.h>
+int main()
+{ int x
+    printf("Hello\n");
+    return 0;
+}#include <stdio.h>
+int main()
+{ int x
+    printf("Hello\n");
+    return 0;
+}#include <stdio.h>
+int main()
+{ int x
+    printf("Hello\n");
+    return 0;
+}#include <stdio.h>
+int main()
+{ int x
+    printf("Hello\n");
+    return 0;
 }
